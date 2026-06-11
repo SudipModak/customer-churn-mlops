0c0c2a7b01bdb483d1df370ed5aff0bf424fd000
SudipModak <sudipmodak719@gmail.com>
2026-06-11 07:39:17 +0530

    Initialize project structure and packaging setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="customer_churn",
+    version="0.0.1",
+    author="Sudip Modak",
+    author_email="SudipModak719@gmail.com",
+    packages=find_packages()
+
+)
